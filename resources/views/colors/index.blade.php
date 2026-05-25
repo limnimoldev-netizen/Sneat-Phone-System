2a194d8728ea48ffb8b3802681ce9e7620ff3e37
vannararin23-sketch <vannararin23@gmail.com>
2026-05-25 15:59:31 +0700

    test

--- resources/views/colors/index.blade.php
+++ resources/views/colors/index.blade.php
@@ -65,6 +65,7 @@
                                     <h5 class="modal-title" id="exampleModalLabel1">{{__('category.add_new')}}</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
+                                
                                     <div class="modal-body">
                                         <div class="row">
                                             <div class="col mb-3">
